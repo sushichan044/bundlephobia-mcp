72c90a14f96cc26d1591a773071cfcc259f37c82
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-05-01 00:33:11 +0900

    add todo

--- packages/cloudflare/src/server/index.ts
+++ packages/cloudflare/src/server/index.ts
@@ -15,6 +15,8 @@ const app = new Hono();
 
 app.mount(
   "/sse",
+  // TODO: fix agents package
+  // eslint-disable-next-line @typescript-eslint/unbound-method
   BundlephobiaAgent.serveSSE("/sse", {
     binding: "BUNDLEPHOBIA_AGENT",
   }).fetch,
@@ -25,6 +27,8 @@ app.mount(
 
 app.mount(
   "/mcp",
+  // TODO: fix agents package
+  // eslint-disable-next-line @typescript-eslint/unbound-method
   BundlephobiaAgent.serve("/mcp", {
     binding: "BUNDLEPHOBIA_AGENT",
   }).fetch,
