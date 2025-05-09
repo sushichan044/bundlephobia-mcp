dc60ca6d56e88e9f5ce6369f023801e39301d93e
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-05-09 19:12:28 +0900

    refactor: add tool directly

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { z } from "zod";
 
+import { errorContentFromPackageHistoryAPIErrorResponse } from "./api/package-history";
+import { fetchPackageHistory } from "./api/package-history";
+import { isPackageHistoryAPIErrorResponse } from "./api/package-history";
+import {
+  errorContentFromSizeAPIErrorResponse,
+  fetchPackageStats,
+  isSizeAPIErrorResponse,
+} from "./api/size";
 import { PKG_NAME, PKG_VERSION } from "./constants";
-import { getNpmPackageInfo, getNpmPackageInfoHistory } from "./tools";
+import { formatPackageHistory, formatPackageHistoryStats } from "./format";
+import { isNonEmptyString } from "./utils/string";
 
 /**
  * Create a new instance of Bundlephobia MCP server.
@@ -15,12 +25,58 @@ export const createServer = (): McpServer => {
   });
 
   server.tool(
-    getNpmPackageInfo.name,
-    getNpmPackageInfo.description,
-    getNpmPackageInfo.schema,
-    async (parameters, extra) => {
+    "get_npm_package_info",
+    [
+      "Get information about an npm package with bundlephobia.",
+      "",
+      "For example, you can retrieve information about:",
+      "- Bundle size",
+      "- Tree-shakeability",
+      "- Dependencies",
+      "- Peer dependencies",
+      "- Assets",
+      "## Usage",
+      "```",
+      "get_npm_package_info(name: '$PACKAGE_NAME')",
+      "```",
+    ].join("\n"),
+    {
+      name: z
+        .string()
+        .describe("The name of the npm package to get information about."),
+    },
+    async ({ name }) => {
       try {
-        return getNpmPackageInfo.handler(parameters, extra);
+        if (!isNonEmptyString(name)) {
+          return {
+            content: [
+              {
+                text: [
+                  "# ❌ Invalid Input Error",
+                  "",
+                  "You must provide a non-empty string.",
+                ].join("\n"),
+                type: "text",
+              },
+            ],
+            isError: true,
+          };
+        }
+
+        const packageInfo = await fetchPackageStats(name);
+
+        if (isSizeAPIErrorResponse(packageInfo)) {
+          return errorContentFromSizeAPIErrorResponse(packageInfo);
+        }
+
+        return {
+          content: [
+            {
+              text: formatPackageHistoryStats(packageInfo),
+              type: "text",
+            },
+          ],
+        };
       } catch (error) {
         console.error(error);
         return {
@@ -43,12 +99,55 @@ export const createServer = (): McpServer => {
   );
 
   server.tool(
-    getNpmPackageInfoHistory.name,
-    getNpmPackageInfoHistory.description,
-    getNpmPackageInfoHistory.schema,
-    async (parameters, extra) => {
+    "get_npm_package_info_history",
+    [
+      "Get all the past information about an npm package stored in bundlephobia.",
+      "",
+      "For example, you can retrieve information about:",
+      "- Bundle size",
+      "- Tree-shakeability",
+      "- Dependencies",
+      "- Peer dependencies",
+      "- Assets",
+      "## Usage",
+      "```",
+      "get_npm_package_info_history(name: '$PACKAGE_NAME')",
+      "```",
+    ].join("\n"),
+    {
+      name: z
+        .string()
+        .describe("The name of the npm package to get information about."),
+    },
+    async ({ name }) => {
       try {
-        return getNpmPackageInfoHistory.handler(parameters, extra);
+        if (!isNonEmptyString(name)) {
+          return {
+            content: [
+              {
+                text: [
+                  "# ❌ Invalid Input Error",
+                  "",
+                  "You must provide a non-empty string.",
+                ].join("\n"),
+                type: "text",
+              },
+            ],
+            isError: true,
+          };
+        }
+        const packageInfo = await fetchPackageHistory(name);
+
+        if (isPackageHistoryAPIErrorResponse(packageInfo)) {
+          return errorContentFromPackageHistoryAPIErrorResponse(packageInfo);
+        }
+
+        return {
+          content: Object.entries(packageInfo).map(([version, pastPkg]) => ({
+            text: formatPackageHistory(version, pastPkg),
+            type: "text",
+          })),
+        };
       } catch (error) {
         console.error(error);
         return {
