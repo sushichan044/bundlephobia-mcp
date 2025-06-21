271b768f8820f754f4aae4a17445925b81ace220
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-06-22 01:50:07 +0900

    docs: add stdio installation guide and cursor deeplink button

---
+++ packages/cloudflare/src/components/InstallToCursor.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  installationLink: string;
+};
+
+export const InstallToCursor = ({ installationLink }: Props) => {
+  return (
+    <a href={installationLink} rel="noopener noreferrer" target="_blank">
+      <img
+        alt="Add bundlephobia MCP server to Cursor"
+        height="32"
+        src="https://cursor.com/deeplink/mcp-install-dark.svg"
+      />
+    </a>
+  );
+};
--- packages/cloudflare/src/hooks/useConfigString.ts
+++ packages/cloudflare/src/hooks/useConfigString.ts
@@ -2,19 +2,51 @@ import { useMemo } from "react";
 
 /**
  * Returns the MCP config as a json snippet or a vscode command to add the config to the current workspace.
- * @param configJson The MCP config as a json object.
+ *
+ * @param name The name of the MCP config.
+ * @param rawConfig The MCP config as a json object without the name.
  */
-export const useMCPConfigSnippet = (configJson: Record<string, unknown>) => {
-  const json = useMemo(() => {
-    return JSON.stringify(configJson, null, 2);
-  }, [configJson]);
+export const useMCPConfigSnippet = (
+  name: string,
+  rawConfig: Record<string, unknown>,
+) => {
+  const jsonWithName = useMemo(() => {
+    return {
+      [name]: rawConfig,
+    };
+  }, [name, rawConfig]);
+
+  const prettyJSON = useMemo(() => {
+    return JSON.stringify(jsonWithName, null, 2);
+  }, [jsonWithName]);
 
   const vscodeCommand = useMemo(() => {
-    return `code --add-mcp '${JSON.stringify(configJson)}'`;
-  }, [configJson]);
+    return `code --add-mcp '${JSON.stringify(jsonWithName)}'`;
+  }, [jsonWithName]);
+
+  const cursorInstallLink = useMemo(() => {
+    return buildCursorInstallLink(name, rawConfig);
+  }, [name, rawConfig]);
 
   return {
-    json,
+    cursorInstallLink,
+    json: prettyJSON,
     vscodeCommand,
   };
+};
+
+/**
+ *
+ * @param name The name of the MCP config.
+ * @param rawConfigJSON The MCP config as a json object without the name.
+ * @returns A link to install the MCP config in Cursor.
+ *
+ * @see {@link https://docs.cursor.com/deeplinks#mcp}
+ */
+const buildCursorInstallLink = (
+  name: string,
+  rawConfigJSON: Record<string, unknown>,
+) => {
+  const base64Config = btoa(JSON.stringify(rawConfigJSON));
+  return `cursor://anysphere.cursor-deeplink/mcp/install?name=${name}&config=${base64Config}`;
 };
