af2fe281fc37c947dfb4b2562536c32b7663abf5
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-06-22 02:03:01 +0900

    style: props order

--- packages/cloudflare/src/components/InstallToCursor.tsx
+++ packages/cloudflare/src/components/InstallToCursor.tsx
@@ -7,9 +7,9 @@ export const InstallToCursor = ({ installationLink }: Props) => {
     <a href={installationLink} rel="noopener noreferrer" target="_blank">
       <img
         alt="Add bundlephobia MCP server to Cursor"
-        width="144"
         height="32"
         src="https://cursor.com/deeplink/mcp-install-dark.svg"
+        width="144"
       />
     </a>
   );
